ad4b8eae401d198fe2791042de61fad172286a99
Artur-Petrov <petrov.artur02@gmail.com>
2025-11-05 00:37:36 +0200

    Initial commit

---
+++ classwork/Lesson1/main.js
@@ -0,0 +1,17 @@
+console.log('123');
+
+let a;
+a = 'hello world';
+console.log(a);
+
+let greeting = 'Hello';
+let intro = 'my name is artur';
+console.log(greeting + ' ' + intro);
+
+let msg2 = `${greeting} ${intro}`;
+console.log(msg2);
+
+console.log(parseInt('1050asd'));
+console.log(+'1050');
+
+console.log(parseFloat('1050.123'));
