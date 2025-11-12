7bf359a6b4d2c9597690aec5925d3769c4846087
Artur-Petrov <petrov.artur02@gmail.com>
2025-11-13 01:07:46 +0200

    first part of HW - 4

---
+++ homework/Lesson4/HW_4.js
@@ -0,0 +1,35 @@
+// #I2XsG6f
+//
+// – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+
+// function calcRectangle (a, b) {
+//     return a * b;
+// }
+// console.log(calcRectangle(4, 8));
+//
+// #ETGAxbEn8l
+//
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+
+// function calcCircle (radius) {
+//     return radius * radius * Math.PI;
+// }
+// console.log(calcCircle(5));
+//
+// #Mbiz5K4yFe7
+//
+// – створити функцію, яка обчислює та повертає площу циліндру висотою h, та радіусом r
+
+// function calcCylinder (radius, height) {
+//     return 2 * Math.PI * radius * (height + radius);
+// }
+// console.log(calcCylinder(3, 5));
+//
+// #SIdMd0hQ
+//
+// – створити функцію, яка приймає масив та виводить кожен його елемент
+// function arrayOutput (arr) {
+//     for (const arrElement of arr) {
+//         console.log(arrElement);
+//     }
+// }
