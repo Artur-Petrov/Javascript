f1a09d4e49a796f89c00ede3c1a74d8f4f2dbddc
Artur-Petrov <petrov.artur02@gmail.com>
2025-11-14 19:47:47 +0200

    done HW - 4

--- homework/Lesson4/HW_4.js
+++ homework/Lesson4/HW_4.js
@@ -149,3 +149,14 @@
 //
 // Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
 
+function exchange(sumUAH,currencyValues,exchangeCurrency){
+    let selectedCurrency;
+    for (const i of currencyValues) {
+        if (i.currency === exchangeCurrency) {
+            selectedCurrency = i;
+        }
+    }
+    return sumUAH/selectedCurrency.value;
+}
+
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'EUR'));
