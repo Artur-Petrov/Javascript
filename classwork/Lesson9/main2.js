cb8c46c6608d615ca564c6ec4bf65eb06abbea83
Artur-Petrov <petrov.artur02@gmail.com>
2025-12-01 20:33:03 +0200

    forms api lesson - 9

---
+++ classwork/Lesson9/main2.js
@@ -0,0 +1,8 @@
+// forms api
+
+console.log(document.forms); // all forms
+// console.log(document.forms['f1'].userName);
+console.log(document.forms['f1']['userName']);
+console.log(document.forms['f2'].userEmail);
+
+console.log(document.forms.f2.userEmail);
