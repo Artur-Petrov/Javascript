424a11906616f4490eeffb1e23ae96cfbfdea621
Artur-Petrov <petrov.artur02@gmail.com>
2025-12-03 01:24:25 +0200

    finished HW - 9

--- homework/Lesson9/HW_9.js
+++ homework/Lesson9/HW_9.js
@@ -101,11 +101,114 @@
 //
 //     Завдання робити через цикли.
 
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+// for (const course of coursesAndDurationArray) {
+//     let div = document.createElement('div');
+//     div.classList.add('item');
+//
+//     let h2 = document.createElement('h2');
+//     h2.classList.add('heading');
+//     h2.innerText = course.title;
+//
+//     let p = document.createElement('p');
+//     p.classList.add('description');
+//     p.innerText = course.monthDuration;
+//
+//     div.append(h2, p);
+//
+//     document.body.appendChild(div);
+// }
+//
+// =========
+//
+// – Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+//
+//
+//
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+//
+// Приклад структури знаходиться у файлі example.png, який лежить в папці з поточним файлом
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
 ];
+
+for (const course of coursesArray) {
+    let wrapperDiv = document.createElement('div');
+    wrapperDiv.classList.add('wrapper');
+
+    let divForTitle = document.createElement('div');
+    divForTitle.classList.add('title');
+    divForTitle.innerText = course.title;
+
+    let divForAllDuration = document.createElement('div');
+    divForAllDuration.classList.add('duration');
+
+    let divForMonthDuration = document.createElement('div');
+    divForMonthDuration.classList.add('month-duration');
+    divForMonthDuration.innerText = course.monthDuration;
+
+    let divForHourDuration = document.createElement('div');
+    divForHourDuration.classList.add('hour-duration');
+    divForHourDuration.innerText = course.hourDuration;
+
+    divForAllDuration.append(divForMonthDuration, divForHourDuration);
+
+    let divForModules = document.createElement('div');
+    divForModules.classList.add('modules');
+
+    let ulForModules = document.createElement('ul');
+    for (const module of course.modules) {
+        let liForModules = document.createElement('li');
+        liForModules.innerText = module;
+        ulForModules.append(liForModules);
+    }
+    divForModules.append(ulForModules);
+
+    wrapperDiv.append(divForTitle, divForAllDuration, divForModules);
+
+    document.body.appendChild(wrapperDiv);
+}
